017a6b65f560852dfc7a3da59259195758f64537
Zack Mitchell <Zack.Mitchell@dpr.co.uk>
2023-09-07 14:58:00 +0100

    Initial Commit

---
+++ CardView.MAUI/PanCardView/Common/Controls/LeftArrowControl.cs
@@ -0,0 +1,24 @@
+﻿using System.ComponentModel;
+using Microsoft.Maui;
+using static PanCardView.Resources.ResourcesInfo;
+
+using AbsoluteLayout = Microsoft.Maui.Controls.Compatibility.AbsoluteLayout;
+
+namespace PanCardView.Controls
+{
+    public class LeftArrowControl : ArrowControl
+    {
+        public LeftArrowControl()
+        {
+            IsRight = false;
+            AbsoluteLayout.SetLayoutBounds(this, new Rect(0, .5, -1, -1));
+        }
+
+        protected override ImageSource DefaultImageSource => WhiteLeftArrowImageSource;
+
+        [EditorBrowsable(EditorBrowsableState.Never)]
+        public new static void Preserve()
+        {
+        }
+    }
+}
---
+++ CardView.MAUI/PanCardView/Common/Controls/RightArrowControl.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.Maui;
+using System.ComponentModel;
+using static PanCardView.Resources.ResourcesInfo;
+
+using AbsoluteLayout = Microsoft.Maui.Controls.Compatibility.AbsoluteLayout;
+
+namespace PanCardView.Controls
+{
+    public class RightArrowControl : ArrowControl
+    {
+        public RightArrowControl()
+        {
+            AbsoluteLayout.SetLayoutBounds(this, new Rect(1, .5, -1, -1));
+        }
+
+        protected override ImageSource DefaultImageSource => WhiteRightArrowImageSource;
+
+        [EditorBrowsable(EditorBrowsableState.Never)]
+        public new static void Preserve()
+        {
+        }
+    }
+}
